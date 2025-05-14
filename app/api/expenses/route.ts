29a91a3af798149d2524829ae2a3f4802dcc5700
joaoc-dev <contact@joao-carvalho.dev>
2025-05-14 02:59:15 +0100

    configure mongoose

--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
+import dbConnect from '@/lib/mongoose';
 
 export async function POST(request: NextRequest) {
+  await dbConnect();
+
   return NextResponse.json({ message: 'Expense created' }, { status: 201 });
 }
