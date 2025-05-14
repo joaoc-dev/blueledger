943ee55a4510b0f5a96522b0a039aefd3445a168
joaoc-dev <contact@joao-carvalho.dev>
2025-05-14 04:01:16 +0100

    add zod to project and implement expenseSchema

--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -1,11 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '@/mongoose/client';
 import Expense from '@/mongoose/models/Expense';
+import { expenseSchema } from '@/schemas/expenseSchema';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = expenseSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.format() },
+      { status: 400 }
+    );
+  }
+
   await dbConnect();
 
-  const { description, price, quantity, totalPrice } = await request.json();
+  const { description, price, quantity, totalPrice } = validation.data;
 
   const expense = await Expense.create({
     description,
