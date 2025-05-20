ed5013b212aed38a00bae5ba25cd4c5d2ab4eca4
joaoc-dev <contact@joao-carvalho.dev>
2025-05-20 13:10:00 +0100

    implements edit expense in webapp

--- app/expenses/(list)/expense-row-actions.tsx
+++ app/expenses/(list)/expense-row-actions.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { TableCell } from '@/components/ui/table';
 import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
@@ -15,28 +16,37 @@ const ExpenseRowActions = ({ id }: { id: string }) => {
     try {
       await axios.delete(`/api/expenses/${id}`);
       router.refresh();
+
       toast.success('Expense deleted successfully');
     } catch (error) {
       console.error(error);
+
       toast.error('Failed to delete expense');
     }
   };
 
   return (
-    <TableCell>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="destructive">Delete</Button>
-        </DialogTrigger>
-        <ConfirmationDialog
-          title="Are you sure you want to delete this expense?"
-          description="This operation is irreversible."
-          onConfirm={() => handleDelete(id)}
-          confirmButtonText="Delete"
-          cancelButtonText="Cancel"
-        />
-      </Dialog>
-    </TableCell>
+    <>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="destructive">Delete</Button>
+          </DialogTrigger>
+          <ConfirmationDialog
+            title="Are you sure you want to delete this expense?"
+            description="This operation is irreversible."
+            onConfirm={() => handleDelete(id)}
+            confirmButtonText="Delete"
+            cancelButtonText="Cancel"
+          />
+        </Dialog>
+      </TableCell>
+      <TableCell>
+        <Button variant="outline" asChild>
+          <Link href={`/expenses/edit/${id}`}>Edit</Link>
+        </Button>
+      </TableCell>
+    </>
   );
 };
 
---
+++ app/expenses/edit/[id]/page.tsx
@@ -0,0 +1,30 @@
+import Expense from '@/mongoose/models/Expense';
+import { notFound } from 'next/navigation';
+import React from 'react';
+import mongoose from 'mongoose';
+import ExpenseForm from '@/app/expenses/_components/expense-form';
+
+const EditExpensePage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+  if (!mongoose.Types.ObjectId.isValid(id)) notFound();
+
+  const expense = await Expense.findById(id);
+  if (!expense) notFound();
+
+  return (
+    <ExpenseForm
+      expense={{
+        id: expense.id,
+        description: expense.description,
+        price: expense.price,
+        quantity: expense.quantity,
+      }}
+    />
+  );
+};
+
+export default EditExpensePage;
