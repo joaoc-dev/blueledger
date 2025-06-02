e940bfdc2c17a67599f5842b1736d314fd81c8d2
joaoc-dev <contact@joao-carvalho.dev>
2025-06-02 14:08:01 +0100

    feat(dashboard/page.tsx): add dashboard page

--- blueledger-webapp/app/page.tsx
+++ blueledger-webapp/app/page.tsx
@@ -3,26 +3,28 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Card className="w-[550px] mx-auto">
-      <CardHeader>
-        <CardTitle>Blue Ledger</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>This will be the landing page for Blue Ledger.</p>
-        <p>A platform for managing your finances.</p>
-        <br />
-        <p>
-          With the help of AI, you can easily track your income and expenses.
-        </p>
-        <br />
-        <p>
-          Go to{' '}
-          <Link href="/expenses" className="underline">
-            Dashboard
-          </Link>{' '}
-          to get started.
-        </p>
-      </CardContent>
-    </Card>
+    <main className="flex justify-center items-center h-screen">
+      <Card className="w-[550px] mx-auto">
+        <CardHeader>
+          <CardTitle>Blue Ledger</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>This will be the landing page for Blue Ledger.</p>
+          <p>A platform for managing your finances.</p>
+          <br />
+          <p>
+            With the help of AI, you can easily track your income and expenses.
+          </p>
+          <br />
+          <p>
+            Go to{' '}
+            <Link href="/dashboard" className="underline">
+              Dashboard
+            </Link>{' '}
+            to get started.
+          </p>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
