dffc4657fd800afaff7e96511885bedb76a6474e
joaoc-dev <contact@joao-carvalho.dev>
2025-06-02 19:04:27 +0100

    feat(nav-bar.tsx): add hover and sliding underline effect to navigation links

--- blueledger-webapp/app/(auth)/nav-bar.tsx
+++ blueledger-webapp/app/(auth)/nav-bar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import { HandCoins } from 'lucide-react';
 import { ThemeToggle } from './theme-toggle';
@@ -27,36 +27,65 @@ const links = [
 
 const NavBar = () => {
   const pathname = usePathname();
+  const activeLinkRef = useRef<HTMLAnchorElement>(null);
+  const navListRef = useRef<HTMLUListElement>(null);
+  const [underlineX, setUnderlineX] = useState(0);
+  const [underlineW, setUnderlineW] = useState(0);
+
+  useEffect(() => {
+    const activeLinkElement = activeLinkRef.current;
+    const navListElement = navListRef.current;
+
+    if (activeLinkElement) {
+      setUnderlineX(activeLinkElement.offsetLeft);
+
+      if (navListElement) {
+        const navListWidth = navListElement.offsetWidth;
+        const linkWidth = activeLinkElement.offsetWidth;
+
+        setUnderlineW(linkWidth / navListWidth);
+      }
+    }
+  }, [pathname]);
 
   return (
-    <nav className="flex items-center justify-between gap-4 p-4">
-      <div className="flex items-center gap-12">
-        <Link href="/" className="flex items-center gap-3">
+    <nav className="nav">
+      <div>
+        <Link href="/" className="nav__brand nav__item hover:text-foreground">
           <HandCoins />
           <span className="text-2xl">Blue Ledger</span>
         </Link>
       </div>
-      <ul className="flex items-center gap-16">
+      <ul
+        className="nav__list"
+        ref={navListRef}
+        style={
+          {
+            '--nav-underline-x': `${underlineX}px`,
+            '--nav-underline-w': `${underlineW}`,
+          } as React.CSSProperties
+        }
+      >
         {links.map((link) => {
           const isActive = pathname === link.href;
-          return renderMenuItem(link, isActive);
+
+          return (
+            <li key={link.label}>
+              <Link
+                ref={isActive ? activeLinkRef : null}
+                className={`nav__item hover:text-foreground ${
+                  isActive ? 'text-foreground' : 'text-muted-foreground'
+                }`}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          );
         })}
       </ul>
       <ThemeToggle />
     </nav>
-  );
-};
-
-const renderMenuItem = (
-  link: { label: string; href: string },
-  isActive: boolean
-) => {
-  return (
-    <li key={link.label}>
-      <Link className={`${isActive ? 'text-blue-600' : ''}`} href={link.href}>
-        {link.label}
-      </Link>
-    </li>
   );
 };
 
