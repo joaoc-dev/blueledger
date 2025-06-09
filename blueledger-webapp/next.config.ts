1e1ab5826504a95e79f88927d3ac4e6642154735
joaoc-dev <contact@joao-carvalho.dev>
2025-06-09 19:04:33 +0100

    build: configure additional sentry env variables

--- blueledger-webapp/next.config.ts
+++ blueledger-webapp/next.config.ts
@@ -9,8 +9,8 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: 'myself-u0d',
-  project: 'blueledger',
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
