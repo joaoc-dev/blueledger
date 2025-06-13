cb6b472852d4563c24293d04f0fb433141438522
joaoc-dev <contact@joao-carvalho.dev>
2025-06-13 14:08:42 +0100

    refactor(expense-schema.ts): remove code duplication

--- blueledger-webapp/lib/validations/expense-schema.ts
+++ blueledger-webapp/lib/validations/expense-schema.ts
@@ -2,57 +2,72 @@ import { Types } from 'mongoose';
 import { z } from 'zod';
 import { EXPENSE_CATEGORIES } from '@/constants/expense-category';
 
-const baseSchema = z.object({
-  id: z.string().optional(),
-  description: z
-    .string()
-    .min(1, { message: 'Description is required' })
-    .max(200, { message: 'Description must be less than 200 characters' }),
-  price: z.number().min(0, { message: 'Price cannot be negative' }),
-  quantity: z.number().min(1, { message: 'Quantity must be greater than 0' }),
-  category: z.enum(EXPENSE_CATEGORIES),
-  date: z.date({
-    required_error: 'Date is required',
-    invalid_type_error: 'Date must be a valid date',
-  }),
+const idSchema = z.string().optional();
+
+const descriptionSchema = z
+  .string()
+  .min(1, { message: 'Description is required' })
+  .max(200, { message: 'Description must be less than 200 characters' });
+
+const priceSchema = z.number().min(0, { message: 'Price cannot be negative' });
+
+const quantitySchema = z
+  .number()
+  .min(1, { message: 'Quantity must be greater than 0' });
+
+const categorySchema = z.enum(EXPENSE_CATEGORIES);
+
+const dateSchema = z.date({
+  required_error: 'Date is required',
+  invalid_type_error: 'Date must be a valid date',
 });
 
-export const createExpenseSchema = baseSchema.extend({
-  date: z
-    .string()
-    .transform((str) => new Date(str))
-    .refine((date) => !isNaN(date.getTime()), {
-      message: 'Invalid date',
-    }),
+const baseFields = {
+  id: idSchema,
+  description: descriptionSchema,
+  price: priceSchema,
+  quantity: quantitySchema,
+  category: categorySchema,
+};
+
+export const expenseFormSchema = z.object({
+  ...baseFields,
+  date: dateSchema,
 });
+
+export type ExpenseFormData = z.infer<typeof expenseFormSchema>;
+
+/* API Schemas */
+const dateStringToDateSchema = z
+  .string()
+  .transform((str) => new Date(str))
+  .refine((date) => !isNaN(date.getTime()), { message: 'Invalid date' });
+
+export const createExpenseSchema = z.object({
+  ...baseFields,
+  date: dateStringToDateSchema,
+});
+
+export type CreateExpenseData = z.infer<typeof createExpenseSchema>;
 
 export const patchExpenseSchema = z.object({
   params: z.object({
     id: z.string().refine(Types.ObjectId.isValid, { message: 'Invalid ID' }),
   }),
-  body: z.object({
-    description: z
-      .string()
-      .min(1, { message: 'Description is required' })
-      .max(200, { message: 'Description must be less than 200 characters' })
-      .optional(),
-    price: z
-      .number()
-      .min(0, { message: 'Price cannot be negative' })
-      .optional(),
-    quantity: z
-      .number()
-      .min(1, { message: 'Quantity must be greater than 0' })
-      .optional(),
-    category: z.enum(EXPENSE_CATEGORIES).optional(),
-    date: z
-      .string()
-      .transform((str) => new Date(str))
-      .refine((date) => !isNaN(date.getTime()), {
-        message: 'Invalid date',
-      })
-      .optional(),
-  }),
+  body: z
+    .object({
+      description: descriptionSchema.optional(),
+      price: priceSchema.optional(),
+      quantity: quantitySchema.optional(),
+      category: categorySchema.optional(),
+      date: dateStringToDateSchema.optional(),
+    })
+    .refine(
+      (data) => Object.values(data).some((value) => value !== undefined),
+      {
+        message: 'At least one field must be provided',
+      }
+    ),
 });
 
 export const deleteExpenseSchema = z.object({
@@ -60,9 +75,3 @@ export const deleteExpenseSchema = z.object({
     id: z.string().refine(Types.ObjectId.isValid, { message: 'Invalid ID' }),
   }),
 });
-
-export const expenseFormSchema = baseSchema.extend({
-  date: z.date(),
-});
-
-export type ExpenseFormData = z.infer<typeof expenseFormSchema>;
