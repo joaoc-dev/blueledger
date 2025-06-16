4a249f7f5338f1b2a4e875c5578c5db0a773178a
joaoc-dev <contact@joao-carvalho.dev>
2025-06-16 18:08:51 +0100

    feat(mobile-nav-menu.tsx): add user panel

--- blueledger-webapp/components/nav-bar/mobile-nav-menu.tsx
+++ blueledger-webapp/components/nav-bar/mobile-nav-menu.tsx
@@ -1,13 +1,25 @@
+'use client';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { Separator } from '../ui/separator';
+import UserAvatar from './user-avatar';
+import { User } from 'next-auth';
+import { BadgeCheck, Bell, CreditCard, LogOut, Sparkles } from 'lucide-react';
 
 interface MobileNavMenuProps {
   links: { label: string; href: string }[];
   isOpen: boolean;
   onClose: () => void;
+  user: User;
 }
 
-const MobileNavMenu = ({ links, isOpen, onClose }: MobileNavMenuProps) => {
+const MobileNavMenu = ({
+  links,
+  isOpen,
+  onClose,
+  user,
+}: MobileNavMenuProps) => {
   const pathname = usePathname();
 
   return (
@@ -36,6 +48,72 @@ const MobileNavMenu = ({ links, isOpen, onClose }: MobileNavMenuProps) => {
             </li>
           );
         })}
+
+        <li className="flex flex-col gap-3">
+          <Separator />
+          <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+            <UserAvatar user={user} />
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-semibold">{user.name}</span>
+              <span className="truncate text-xs">{user.email}</span>
+            </div>
+          </div>
+          <Separator />
+        </li>
+
+        <li className="flex flex-col gap-3">
+          <Link
+            onClick={onClose}
+            href={pathname}
+            className="flex items-center gap-2"
+          >
+            <Sparkles />
+            Upgrade to Pro(soon)
+          </Link>
+          <Separator />
+        </li>
+
+        <li>
+          <Link
+            onClick={onClose}
+            href="/user/profile"
+            className="flex items-center gap-2"
+          >
+            <BadgeCheck />
+            Profile
+          </Link>
+        </li>
+        <li>
+          <Link
+            onClick={onClose}
+            href={pathname}
+            className="flex items-center gap-2"
+          >
+            <CreditCard />
+            <span>Billing(soon)</span>
+          </Link>
+        </li>
+        <li className="flex flex-col gap-3">
+          <Link
+            onClick={onClose}
+            href={pathname}
+            className=" flex items-center gap-2"
+          >
+            <Bell />
+            Notifications(soon)
+          </Link>
+          <Separator />
+        </li>
+        <li>
+          <Link
+            onClick={onClose}
+            href={pathname}
+            className="flex items-center gap-2"
+          >
+            <LogOut />
+            Logout
+          </Link>
+        </li>
       </ul>
       {isOpen && (
         <div
---
+++ blueledger-webapp/types/user.ts
@@ -0,0 +1,9 @@
+export interface UserType {
+  id: string;
+  name: string;
+  email: string;
+  image: string;
+  bio: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
