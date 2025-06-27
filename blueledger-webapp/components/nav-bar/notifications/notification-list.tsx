7856581ff463ed46ab0dc8e8df56a5547b3be7c2
joaoc-dev <contact@joao-carvalho.dev>
2025-06-27 22:44:19 +0100

    feat: add smooth animations to notification list

--- blueledger-webapp/components/nav-bar/notifications/notification-list.tsx
+++ blueledger-webapp/components/nav-bar/notifications/notification-list.tsx
@@ -1,5 +1,6 @@
 import { NotificationWithUser } from '@/lib/data/notifications.mock';
 import { ScrollArea } from '@/components/custom/scroll-area';
+import { AnimatePresence, motion } from 'motion/react';
 import { NotificationItem } from './notification-item';
 
 export const NotificationList = ({
@@ -12,15 +13,25 @@ export const NotificationList = ({
   return (
     <div>
       <ScrollArea className="h-96 w-full">
-        {notificationsList.map((n) => {
-          return (
-            <NotificationItem
-              key={n.id}
-              notification={n}
-              setNotificationRead={setNotificationRead}
-            />
-          );
-        })}
+        <AnimatePresence>
+          {notificationsList.map((n) => {
+            return (
+              <motion.div
+                key={n.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, height: 'auto' }}
+                exit={{ opacity: 0, height: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <NotificationItem
+                  key={n.id}
+                  notification={n}
+                  setNotificationRead={setNotificationRead}
+                />
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
       </ScrollArea>
     </div>
   );
