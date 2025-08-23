94565cc6aefbec80b0e85dd4eed58b131a75cef6
joaoc-dev <contact@joao-carvalho.dev>
2025-08-23 16:06:22 +0100

    test: add api-client tests

--- blueledger-webapp/tests/mocks/handlers.ts
+++ blueledger-webapp/tests/mocks/handlers.ts
@@ -1,6 +1,10 @@
 import type { HttpHandler } from 'msw';
+import { errorHandlers } from './handlers/error-handlers';
 import { expenseHandlers } from './handlers/expense-handlers';
 
-const handlers: HttpHandler[] = [...expenseHandlers];
+const handlers: HttpHandler[] = [
+  ...errorHandlers,
+  ...expenseHandlers,
+];
 
 export { handlers };
